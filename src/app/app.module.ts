2bf61bd7619f4292c3ecb7d9e5caaa8a85c345d1
Ahlem FANTAZI <fantaziahlem@gmail.com>
2021-01-21 01:25:18 +0100

    navigation with routing added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,32 @@ import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import {HomeComponent} from './shared/home/home.component';
 import {AppComponent } from './app.component';
 import {RouterModule, Routes} from '@angular/router';
+import {FooterComponent} from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
+import {HttpClient} from '@angular/common/http';
+import {LoginComponent} from './user/login/login.component';
+import {SigninComponent} from './user/signin/signin.component';
 
 const appRoutes: Routes = [
   {
     path: '',
     component: HomeComponent
-  }
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'signin',
+    component: SigninComponent
+  },
 ];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     RouterModule.forRoot(appRoutes),
@@ -21,7 +37,7 @@ const appRoutes: Routes = [
     RouterModule
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  providers: [],
+  providers: [HttpClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginComponentComponent } from './login-component/login-component.component';
+import { SignInComponentComponent } from './sign-in-component/sign-in-component.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { LoginComponent } from './login/login.component';
+import { SigninComponent } from './signin/signin.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [LoginComponentComponent, SignInComponentComponent, SignInComponent, LoginComponent, SigninComponent],
   imports: [
     CommonModule
   ]
