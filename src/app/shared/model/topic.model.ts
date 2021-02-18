9c0fda8547e9b025219cbb12e73898e85f6450bd
Ahlem FANTAZI <fantaziahlem@gmail.com>
2021-02-19 00:03:22 +0100

    creating MatchMethods const

--- src/app/shared/model/topic.model.ts
+++ src/app/shared/model/topic.model.ts
@@ -3,3 +3,9 @@ export interface Topic {
   name?:string;
 }
 
+export const MatchMethods = {
+  SCOREDOUBLE: { libelle: 'scoreDouble', icon: "../assets/icon/ScoreResult.png"},
+  SEEKEDDOUBLE: { libelle: 'seekedDouble', icon: "../assets/icon/AffinityResult.png"},
+} as const
+
+export type Methods = keyof typeof MatchMethods
